e59eebb5834b276a1f82fbc62cba48421175aed2
subashprj55 <subashprj55@gmail.com>
2025-01-23 09:11:48 +0545

    initailized project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "antd";
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
         >
           Learn React
         </a>
+        <Button>test</Button>
+        <p className="text-5xl">this is ttest also</p>
       </header>
     </div>
   );
